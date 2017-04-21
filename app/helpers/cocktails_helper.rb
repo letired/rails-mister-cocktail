9faa22b537810389f4067b4b442a35e52214efcf
Spencer Bray <spencerjohnbray@gmail.com>
2017-04-21 14:58:41 +0200

    added logic to index to improve display previewing

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,2 +1,15 @@
 module CocktailsHelper
+  def three_doses(cocktail)
+    cocktail.doses.all.first(3)
+  end
+
+  def only_three(cocktail)
+    if cocktail.doses.all.first(3).count < cocktail.doses.all.count
+      return "..."
+    end
+  end
+
+  def doses?(cocktail)
+    cocktail.doses.all.first
+  end
 end
